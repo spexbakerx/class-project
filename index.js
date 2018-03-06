0f3a36cf03250068ce504622ce4d558cb8a43186
Lauren Baker <spexbakerx@gmail.com>
2018-03-06 12:10:38 -0800

    html structure started

---
+++ index.js
@@ -0,0 +1,24 @@
+// console.log("hello");
+
+
+// const apiUrl = "https://dog.ceo/api/breeds/list"
+
+
+// function getApi() {
+// 	$.ajax ({
+// 		url: apiUrl,
+// 		method: 'GET',
+// 		success: function(response) {
+// 				console.log(response);
+// 			response.message.forEach(function(message){
+// 				$("#js-results").append(`<li>${message}</li>`);
+
+// 			})
+// 		}
+
+// 	})
+// }
+
+// getApi();
+
+
