a5e9e0acfb66e92ae76fcfe14e76b80e18672d17
Lauren Baker <spexbakerx@gmail.com>
2018-03-07 19:56:56 -0800

    css styling underway

--- index.js
+++ index.js
@@ -21,4 +21,49 @@
 
 // getApi();
 
+// ------- smooth scroll ------- //
+
+$(function() {
+	$('.section a').bind('click',function(event){
+		var $anchor = $(this);
+
+		$('html, body').stop().animate({
+			scrollTop: $($anchor.attr('href')).offset().top
+		}, 1500,'easeInOutExpo');
+		/*
+		if you don't want to use the easing effects:
+		$('html, body').stop().animate({
+			scrollTop: $($anchor.attr('href')).offset().top
+		}, 1000);
+		*/
+		event.preventDefault();
+	});
+});
+
+
+// ------- keeps track of where mouse is ------- //
+
+document.querySelector('.button.colorful').onmousemove = (e) => {
+  const x = e.pageX - e.target.offsetLeft
+  const y = e.pageY - e.target.offsetTop
+  e.target.style.setProperty('--x', `${ x }px`)
+  e.target.style.setProperty('--y', `${ y }px`)
+ 
+}
+
+$( "input" ).on( "click", function() {
+        $(this).find(".center").css({opacity: 0.0});
+    }, function() {
+         $(this).find(".center").css({opacity: 0.0});
+    });
+
+
+ // $(".center").css("opacity", 1);
+ //    $("label").hover(function() {
+ //        $(this).find(".center").animate({opacity: 0.0}, 200);
+ //    }, function() {
+ //         $(this).find(".center").animate({opacity: 1}, 200);
+ //    });
+
+
 
